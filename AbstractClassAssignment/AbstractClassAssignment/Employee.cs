d7b836b1708566c9971310d4daabd1e62fce27cc
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-25 18:10:55 +1200

    Added Abstract Class Assignment
    Added AbstractClassAssignment and all associated files

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public class Employee : Person // Employee class inherits from 'Person' class
+    {
+        public override void SayName() // Instantiates SayName from abstract class Person
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName); // Displays values of properties to user
+        }
+    }
+}
