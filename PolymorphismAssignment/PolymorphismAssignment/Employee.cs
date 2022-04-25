b35cfdf8322c87413f4f5f9fc74735881f48e0a8
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-25 18:46:46 +1200

    Added Polymorphism Assignment
    Added PolymorphismAssignment and all associated files

---
+++ PolymorphismAssignment/PolymorphismAssignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    public class Employee : Person, IQuittable // Employee class inherits from 'Person' class, and 'IQuittable' interface
+    {
+        public override void SayName() // Instantiates SayName from abstract class Person
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName); // Displays values of properties to user
+        }
+
+        public void Quit() // Instantiates Quit from interface IQuittable
+        {
+            // Displays message to user
+            Console.WriteLine(FirstName + " " + LastName + " slaps their resignation letter on the desk!");
+            Console.WriteLine(FirstName + ": \"I'm outta here, man!!!\"");
+            Console.WriteLine(FirstName + " " + LastName + " has quit their job!");
+        }
+    }
+}
---
+++ PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uses polymorphism to instantiate IQuittable/Employee object 'ron' and assigns values to properties
+            IQuittable ron = new Employee() { FirstName = "Ron", LastName = "Obvious" };
+
+            ron.Quit(); // Utilises interface method 'Quit' on employee object
+
+            Console.ReadLine(); // Prevents program from terminating immediately
+        }
+    }
+}
