794eda2c88bc54a5430ecaa882ce539452c748f3
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-25 16:22:17 +1200

    Added Class Assignment
    Added ClassAssignment and all associated files

---
+++ ClassAssignment/ClassAssignment/Maths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public static class Maths
+    {
+        public static void Maths1(int num) // Creates method 'Maths1' with int parameter
+        {
+            int result = num / 2; // Divides int argument and saves result in int variable
+            Console.WriteLine(num + " divided by 2 = " + result + "\n"); // Displays equation and result to user
+        }
+
+        public static void Maths1(string num) // Overloads 'Maths1' method with string parameter
+        {
+            int number = Convert.ToInt32(num); // Converts string argument to int variable
+            int result = number * 24; // Multiplies int argument and saves result in int variable
+            Console.WriteLine(number + " multiplied by 24 = " + result + "\n"); // Displays equation and result to user
+        }
+
+        public static void Maths2(out int i) // Creates 'Maths2' method with output parameter
+        {
+            i = 10; // Provides value to output varible
+            i *= i; // Multiplies variable i by itself and changes value of i
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do some math!"); // Displays message to user and requests appropriate input
+            Console.WriteLine("Please provide a whole number:");
+            string num = Console.ReadLine(); // Saves user input into a string variable called 'num'
+            int num2 = Convert.ToInt32(num); // Converts string 'num' to int variable called 'num2'
+            int x; // Declares an int variable called 'x'
+
+
+            Maths.Maths1(num2); // Calls 'Maths1' method and provides int argument
+
+            Maths.Maths1(num); // Calls 'Maths1' method and provides string argument
+
+            Maths.Maths2(out x); // Calls 'Maths2' method and utilises output argument
+            Console.WriteLine("Variable value: {0}", x); // Displays value of output variable 'x'
+
+
+            Console.ReadLine(); // Prevents program from terminating immediately
+
+        }
+    }
+}
