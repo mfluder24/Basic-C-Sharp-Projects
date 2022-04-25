a39c7307dd2e03c78908aa39bc0509dc2cc42c72
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-26 09:54:47 +1200

    Updated 1 file
    Updated Program.cs

--- MainMethodAssignment/MainMethodAssignment/Program.cs
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -13,7 +13,7 @@ namespace MainMethodAssignment
             var obj = new Maths(); // Instantiates new instance of Maths class
             int num = 17; // Creates an int variable called 'num' with a value of 17
             decimal number = 1408; // Creates a decimal variable called 'number' with a value of 1408
-            string integer = "42"; // Creates a string variable called 'integer' with a value of "42"
+            string strNum = "42"; // Creates a string variable called 'strNum' with a value of "42"
 
             Console.WriteLine("Let's do some math!"); // Displays a message to the user
 
@@ -21,10 +21,10 @@ namespace MainMethodAssignment
             Console.WriteLine(num + " plus 24 = " + result1); // Displays equation to user and provides result
 
             int result2 = obj.Maths1(number); // Calls 'Maths1' method and provides decimal argument, saves returned result as int 
-            Console.WriteLine(num + " divided by 2 = " + result2); // Displays equation to user and provides result
+            Console.WriteLine(number + " divided by 2 = " + result2); // Displays equation to user and provides result
 
-            int result3 = obj.Maths1(integer); // Calls 'Maths1' method and provides string argument, saves returned result as int
-            Console.WriteLine(number + " multiplied by 3 = " + result3); // Displays equation to user and provides result
+            int result3 = obj.Maths1(strNum); // Calls 'Maths1' method and provides string argument, saves returned result as int
+            Console.WriteLine(strNum + " multiplied by 3 = " + result3); // Displays equation to user and provides result
 
             Console.ReadLine(); // Prevents program from terminating immediately
         }
