e66d0982709cb461169f4f0fb1dbfb7838f2d26b
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-25 12:54:56 +1200

    Added Main Method Assignment
    Added MainMethodAssignment and all associated files

---
+++ MainMethodAssignment/MainMethodAssignment/Maths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Maths
+    {
+        public static void Maths1(int num) // Creates method called 'Maths1' with int parameter
+        {
+            int result = num + 24; // Performs math operation on num argument and saves result as int variable
+            Console.WriteLine(num + " plus 24 = " + result); // Displays equation to user and provides result
+        }
+
+        public static void Maths1(decimal num) // Creates method called 'Maths1' with decimal parameter
+        {
+            int result = Convert.ToInt32(num / 2); // Performs equation on decimal argument and converts result to int variable
+            Console.WriteLine(num + " divided by 2 = " + result); // Displays equation to user and provides result
+        }
+
+        public static void Maths1(string num) // Creates method called 'Maths1' with string parameter
+        {
+            int number = Convert.ToInt32(num); // Converts string argument to int variable
+            int result = number * 3; // Performs equation on int argument and saves result to int variable
+            Console.WriteLine(number + " multiplied by 3 = " + result); // Displays equation to user and provides result
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 17; // Creates an int variable called 'num' with a value of 17
+            decimal number = 1408; // Creates a decimal variable called 'number' with a value of 1408
+            string integer = "42"; // Creates a string variable called 'integer' with a value of "42"
+
+            Console.WriteLine("Let's do some math!"); // Displays a message to the user
+
+            Maths.Maths1(num); // Calls 'Maths1' method and provides int argument
+
+            Maths.Maths1(number); // Calls 'Maths1' method and provides decimal argument 
+
+            Maths.Maths1(integer); // Calls 'Maths1' method and provides string argument
+
+            Console.ReadLine(); // Prevent program from terminating immediately
+        }
+    }
+}
