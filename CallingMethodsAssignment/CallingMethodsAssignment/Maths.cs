bc994549568162fe8162042a5614ef56d8645834
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-25 12:08:54 +1200

    Added Calling Methods Assignment
    Added CallingMethodsAssignment and all associated files

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Maths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Maths 
+    {
+        public static void Maths1(int num) // Creates method 'Maths1' and has parameters of int variable 
+        {
+            int result = num + 20; // Adds 20 to int argument and saves 'result' as int variable
+            Console.WriteLine(num + " plus 20 = "+ result + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
+        }
+
+        public static void Maths2(int num) // Creates method 'Maths2' and has parameters of int variable
+        {
+            int result = num - 5; // Subtracts 5 from int argument and saves 'result' as int variable
+            Console.WriteLine(num + " minus 5 = " + result + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
+        }
+
+        public static void Maths3(int num) // Creates method 'Maths3' and has parameters of int variable
+        {
+            int result = num * 17; // Multiplies int argument by 17 and saves 'result' as int variable
+            Console.WriteLine(num + " multiplied by 17 = " + result + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do some math!"); // Displays message to user and prompts for input
+            Console.WriteLine("Please provide a whole number to do math with:"); 
+            int num = Convert.ToInt32(Console.ReadLine()); // Saves user input in a string variable and converts it to an int variable
+
+            Maths.Maths1(num); // Calls Maths1 method and uses user input 'num' as argument
+
+            Maths.Maths2(num); // Calls Maths2 method and uses user input 'num' as argument
+
+            Maths.Maths3(num); // Calls Maths3 method and uses user input 'num' as argument
+
+            Console.WriteLine("Thanks for doing some math with us!"); // Displays thank you message to user
+            Console.ReadLine(); // Prevents the program from terminating immediately
+        } 
+
+    }
+}
