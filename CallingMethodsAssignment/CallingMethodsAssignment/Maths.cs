ee54dc29506760e938f41b00910dc0454b5fbe6e
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-26 09:05:23 +1200

    Updated Two Files
    Updated CallingMethodsAssignment.cs and Maths.cs

--- CallingMethodsAssignment/CallingMethodsAssignment/Maths.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment/Maths.cs
@@ -8,22 +8,19 @@ namespace CallingMethodsAssignment
 {
     public class Maths 
     {
-        public static void Maths1(int num) // Creates method 'Maths1' and has parameters of int variable 
+        public int Maths1(int num) // Creates method 'Maths1' and has parameters of int variable 
         {
-            int result = num + 20; // Adds 20 to int argument and saves 'result' as int variable
-            Console.WriteLine(num + " plus 20 = "+ result + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
+            return num + 20; // Adds 20 to int argument and returns result
         }
 
-        public static void Maths2(int num) // Creates method 'Maths2' and has parameters of int variable
+        public int Maths2(int num) // Creates method 'Maths2' and has parameters of int variable
         {
-            int result = num - 5; // Subtracts 5 from int argument and saves 'result' as int variable
-            Console.WriteLine(num + " minus 5 = " + result + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
+            return num - 5; // Subtracts 5 from int argument and returns result
         }
 
-        public static void Maths3(int num) // Creates method 'Maths3' and has parameters of int variable
+        public int Maths3(int num) // Creates method 'Maths3' and has parameters of int variable
         {
-            int result = num * 17; // Multiplies int argument by 17 and saves 'result' as int variable
-            Console.WriteLine(num + " multiplied by 17 = " + result + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
+            return num * 17; // Multiplies int argument by 17 and returns
         }
     }
 }
--- CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -10,15 +10,19 @@ namespace CallingMethodsAssignment
     {
         static void Main(string[] args)
         {
+            var obj = new Maths(); // Instantiates Maths class 
             Console.WriteLine("Let's do some math!"); // Displays message to user and prompts for input
             Console.WriteLine("Please provide a whole number to do math with:"); 
             int num = Convert.ToInt32(Console.ReadLine()); // Saves user input in a string variable and converts it to an int variable
 
-            Maths.Maths1(num); // Calls Maths1 method and uses user input 'num' as argument
+            int result1 = obj.Maths1(num); // Calls Maths1 method and uses user input 'num' as argument, saves result to int variable
+            Console.WriteLine(num + " plus 20 = " + result1 + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
 
-            Maths.Maths2(num); // Calls Maths2 method and uses user input 'num' as argument
+            int result2 = obj.Maths2(num); // Calls Maths2 method and uses user input 'num' as argument, saves result to int variable
+            Console.WriteLine(num + " minus 5 = " + result2 + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
 
-            Maths.Maths3(num); // Calls Maths3 method and uses user input 'num' as argument
+            int result3 = obj.Maths3(num); // Calls Maths3 method and uses user input 'num' as argument, saves result to int variable
+            Console.WriteLine(num + " multiplied by 17 = " + result3 + "\n"); // Displays equation and result to user, ending with new line to keep things tidy
 
             Console.WriteLine("Thanks for doing some math with us!"); // Displays thank you message to user
             Console.ReadLine(); // Prevents the program from terminating immediately
