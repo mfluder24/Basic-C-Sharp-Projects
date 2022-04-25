87ac23b31d036bf439b5f76443931d982c32e669
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-25 13:34:10 +1200

    Added Method Assignment
    Added MethodAssignment and all associated files

---
+++ MethodAssignment/MethodAssignment/Maths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Maths
+    {
+        // Creates 'Maths1' method with one required parameter (num) and num2 set to 5, which can be changed with optional user input
+        public static void Maths1(int num, int num2 = 5)
+        { 
+            int result = num * num2; // Multipies num by num2 and saves result as int variable
+            Console.WriteLine(num + " plus " + num2 + " = " + result); // Displays equation and result to user
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do some math!"); // Displays message to user and prompts for appropriate input
+            Console.WriteLine("Please provide a whole number:");
+            int num1 = Convert.ToInt32(Console.ReadLine()); // Saves user input as string and converts it to an int variable
+
+            Console.WriteLine("Please provide another whole number (optional, can leave blank):"); // Displays message to user and prompt for optional appropriate input
+            string answer = Console.ReadLine(); // Saves user input as string variable
+            if (answer != "") // If statement that will execute if user input a number
+            {
+                int num2 = Convert.ToInt32(answer); // Converts string 'answer' to an int variable
+                Maths.Maths1(num1, num2); // Calls 'Maths1' method and provides two arguments
+            }
+            else // Else statement that executes if user left optional number blank
+            {
+                Maths.Maths1(num1); // Calls 'Maths1' method and provides one argument
+            }
+
+            Console.ReadLine(); // Prevvents program from terminating immediately
+        }
+    }
+}
