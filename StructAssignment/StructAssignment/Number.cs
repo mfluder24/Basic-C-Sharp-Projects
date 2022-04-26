ca566cf482010c8834a351e26fc5f48e1e5a2f36
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-26 16:19:19 +1200

    Added Struct Assignment
    Added StructAsssignment and all associated files

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public struct Number // Struct Number
+    {
+        public decimal Amount { get; set; } // Assigns struct a decimal property called 'Amount'
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number() { Amount = 1408 }; // Creates new Number 'num' and sets Amount to 1408
+
+            Console.WriteLine(num.Amount); // Displays 'Amount' to user
+
+            Console.ReadLine(); // Prevents program from terminating immediately
+            
+        }
+    }
+}
