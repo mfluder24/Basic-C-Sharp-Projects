fee414da5736e51a951368b2378ab9fd5b760d5b
Fox <99509225+mfluder24@users.noreply.github.com>
2022-04-30 16:05:40 +1200

    Added Constructor Assignment
    Added ConstructorAssignment and all associated files

---
+++ ConstructorAssignment/ConstructorAssignment/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Class1
+    {
+        public int Wallet { get; set; }
+        public string Name { get; set; }
+
+        public Class1(string name, int wallet)
+        {
+            Wallet = wallet;
+            Name = name;
+        }
+
+        public Class1(string name) : this(name, 50)
+        {
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+       
+    static void Main(string[] args)
+        {
+            const string person = "Walter White"; // Creates a string constant 
+
+            var welcome = "Welcome "; // Uses 'var' to create a string variable
+
+            Console.WriteLine(welcome + person + "!"); // Displays const and var to show they work
+
+            Class1 newClass1 = new Class1("Jesse Pinkman"); // Uses chained constructor to create a new instance of Class1
+
+            Console.WriteLine(newClass1.Name + " " + newClass1.Wallet); // Displays properties of 'newClass1' instance to show chained constructor worked
+
+            Console.ReadLine(); // Prevents program from terminating immediately
+      
+        }
+    }
+}
